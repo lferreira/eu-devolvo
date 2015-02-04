8ec3ccb79c8ee9ea7d3f367bc0dd1a6053ad49b4
Luiz Ferreira <luizhgferreira@gmail.com>
2015-02-04 20:03:44 -0200

    running on heroku

--- src/main/java/com/dploy/eudevolvo/Application.java
+++ src/main/java/com/dploy/eudevolvo/Application.java
@@ -1,10 +1,18 @@
 package com.dploy.eudevolvo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
+@RestController
+@SpringBootApplication
 public class Application {
 
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+	
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
