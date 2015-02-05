dff3a59758be315d0c03043199afe144bec66092
Luiz Ferreira <luizhgferreira@gmail.com>
2015-02-05 00:07:29 -0200

    app is ready for work

--- src/main/java/com/dploy/eudevolvo/Application.java
+++ src/main/java/com/dploy/eudevolvo/Application.java
@@ -1,4 +1,5 @@
 package com.dploy.eudevolvo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
